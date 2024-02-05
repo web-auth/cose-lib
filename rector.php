bf3c7593d2724ad125d012b0c04dcbc74dcd23a5
Florent Morselli <florent.morselli@spomky-labs.com>
2024-02-05 21:53:39 +0100

    Deps updated

--- rector.php
+++ rector.php
@@ -3,22 +3,18 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Core\ValueObject\PhpVersion;
-use Rector\DeadCode\Rector\ClassMethod\RemoveUnusedPrivateMethodParameterRector;
 use Rector\Doctrine\Set\DoctrineSetList;
-use Rector\Php81\Rector\Property\ReadOnlyPropertyRector;
 use Rector\PHPUnit\CodeQuality\Rector\Class_\PreferPHPUnitThisCallRector;
-use Rector\PHPUnit\Set\PHPUnitLevelSetList;
 use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
-use Rector\Symfony\Set\SymfonyLevelSetList;
 use Rector\Symfony\Set\SymfonySetList;
+use Rector\ValueObject\PhpVersion;
 
 return static function (RectorConfig $config): void {
     $config->import(SetList::DEAD_CODE);
     $config->import(LevelSetList::UP_TO_PHP_81);
-    $config->import(SymfonyLevelSetList::UP_TO_SYMFONY_60);
+    $config->import(SymfonySetList::SYMFONY_64);
     $config->import(SymfonySetList::SYMFONY_50_TYPES);
     $config->import(SymfonySetList::SYMFONY_52_VALIDATOR_ATTRIBUTES);
     $config->import(SymfonySetList::SYMFONY_CODE_QUALITY);
@@ -28,9 +24,9 @@ return static function (RectorConfig $config): void {
     $config->import(DoctrineSetList::ANNOTATIONS_TO_ATTRIBUTES);
     $config->import(PHPUnitSetList::PHPUNIT_CODE_QUALITY);
     $config->import(PHPUnitSetList::ANNOTATIONS_TO_ATTRIBUTES);
-    $config->import(PHPUnitLevelSetList::UP_TO_PHPUNIT_100);
+    $config->import(PHPUnitSetList::PHPUNIT_100);
     $config->paths([__DIR__ . '/src', __DIR__ . '/tests']);
-    $config->services()->remove(PreferPHPUnitThisCallRector::class);
+    $config->skip([PreferPHPUnitThisCallRector::class]);
     $config->phpVersion(PhpVersion::PHP_81);
     $config->parallel();
     $config->importNames();
